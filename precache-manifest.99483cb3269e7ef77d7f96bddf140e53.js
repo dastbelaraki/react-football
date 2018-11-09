9775452a467b37c6cbcaed7956a17078adfb5261
imanDB <iman.dastbelaraki@gmail.com>
2018-11-09 12:57:27 +0330

    Updates

---
+++ precache-manifest.99483cb3269e7ef77d7f96bddf140e53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e055fb491632cd7be494535d08a13e1",
+    "url": "/react-football/static/media/Bundesliga.3e055fb4.png"
+  },
+  {
+    "revision": "33f0b0002ceaebded80a",
+    "url": "/react-football/static/css/main.7c91c03c.chunk.css"
+  },
+  {
+    "revision": "e442c301e5eed9a54f69",
+    "url": "/react-football/static/js/1.e442c301.chunk.js"
+  },
+  {
+    "revision": "1fe8cf5926b2d338963a",
+    "url": "/react-football/static/js/runtime~main.1fe8cf59.js"
+  },
+  {
+    "revision": "2be242902f8f7daeaf6ff1f729b60ad1",
+    "url": "/react-football/static/media/LaLiga.2be24290.png"
+  },
+  {
+    "revision": "33f0b0002ceaebded80a",
+    "url": "/react-football/static/js/main.33f0b000.chunk.js"
+  },
+  {
+    "revision": "0723988637eb0bcceb4564d8647b8599",
+    "url": "/react-football/static/media/Eredivisie.07239886.png"
+  },
+  {
+    "revision": "4b2af31f18df85fc286831937615225b",
+    "url": "/react-football/static/media/Ligue1.4b2af31f.png"
+  },
+  {
+    "revision": "2ea36f1c21b081213f172410bba0355b",
+    "url": "/react-football/static/media/PremierLeague.2ea36f1c.png"
+  },
+  {
+    "revision": "3ee74dc22ac21026487a24b49cf2ccfa",
+    "url": "/react-football/static/media/SerieA.3ee74dc2.png"
+  },
+  {
+    "revision": "2477fe1b9ff25b5e3ff7e7a5c44a9aad",
+    "url": "/react-football/index.html"
+  }
+];
